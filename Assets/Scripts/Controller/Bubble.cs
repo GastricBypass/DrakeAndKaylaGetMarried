1d1f95408703056d721cc1bb0cad5042aabc5ef3
GastricBypass <dsp45@case.edu>
2022-11-27 17:46:10 -0500

    Bubbled characters can no longer unbubble other characters. Also there's now a 1 second delay to when your bubble can be popped.

--- Assets/Scripts/Controller/Bubble.cs
+++ Assets/Scripts/Controller/Bubble.cs
@@ -5,12 +5,50 @@ using UnityEngine;
 public class Bubble : MonoBehaviour
 {
     public Character Character;
+    public float EnableTime = 1;
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    private bool _enabled = false;
+    private Player _currentPlayerInBubble = null;
+
+    private void OnEnable()
     {
-        if (collision.GetComponent<Player>() != null)
+        StartCoroutine(WaitToEnable(EnableTime));
+    }
+
+    private void Update()
+    {
+        if (_enabled && _currentPlayerInBubble != null && !_currentPlayerInBubble.IsDead)
         {
             Character.SetBubbleEnabled(false);
         }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player != null && !player.IsDead) 
+        {
+            _currentPlayerInBubble = player;
+            if (_enabled)
+            {
+                Character.SetBubbleEnabled(false);
+            }
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            _currentPlayerInBubble = null;
+        }
+    }
+
+    private IEnumerator WaitToEnable(float seconds)
+    {
+        _enabled = false;
+        yield return new WaitForSeconds(seconds);
+        _enabled = true;
     }
 }
