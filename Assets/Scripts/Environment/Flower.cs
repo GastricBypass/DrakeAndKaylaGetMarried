b39e1a43024d9ae4610657ac55f995a412b74ea8
GastricBypass <dsp45@case.edu>
2023-05-29 20:14:48 -0700

    New cloud, new trees, new segments. Got rid of some segments I decided were bad. Also I'm not using prefab variants as intensely anymore.

--- Assets/Scripts/Environment/Flower.cs
+++ Assets/Scripts/Environment/Flower.cs
@@ -7,6 +7,14 @@ public class Flower : MonoBehaviour
     public MovingItem Parent;
     public int Points = 2;
 
+    private void Start()
+    {
+        if (Parent == null)
+        {
+            Parent = GetComponentInParent<MovingItem>();
+        }
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Player>() != null)
